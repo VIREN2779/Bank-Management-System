ef203e980f2344d9eec2e916cd9ecdff2f737f4a
VIREN2779 <virenpatadiya073@gmail.com>
2025-06-09 18:48:36 +0530

    #3 Update index.html and remove unused react.svg asset

--- frontend/src/components/login-form.jsx
+++ frontend/src/components/login-form.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import Img from "../assets/login-page-img.png"
 
 export function LoginForm({
   className,
@@ -78,7 +79,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
+              src={Img}
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
           </div>
